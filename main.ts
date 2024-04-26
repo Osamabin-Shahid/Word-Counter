d33878481162622f2de4fd654175c0ca9382a9f1
Osamabin-Shahid <obsmr23@gmail.com>
2024-04-26 13:44:41 +0500

    Initial upload

---
+++ main.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(
+  chalk.bgMagenta(
+    "====================>>Welcome to the Word Counter<<================"
+  )
+);
+let condition: boolean = true;
+while (condition) {
+  const answer = await inquirer.prompt([
+    {
+      name: "sentence",
+      type: "input",
+      message: chalk.magenta("Enter a sentence for counting of its words"),
+    },
+    {
+      name: "again",
+      type: "confirm",
+      message: chalk.blue("If you want to check again then type YES"),
+      default: "false",
+    },
+  ]);
+
+  const words = answer.sentence.trim().split(" ");
+  console.log(chalk.red(words));
+  condition = answer.again;
+
+  console.log(
+    chalk.bgGrey.bold.yellow(`Your sentence have total ${words.length} words`)
+  );
+}
+
